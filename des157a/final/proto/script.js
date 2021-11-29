6b6668f3139b977788de17897fa661e6f30f79f3
ShengHao Shek <louisshek@ShengHaodeMacBook-Pro.local>
2021-11-29 12:30:44 -0800

    Final Design usertest Created

--- des157a/final/proto/script.js
+++ des157a/final/proto/script.js
@@ -3,14 +3,17 @@
 setTimeout(function(){
     document.getElementById("start").style.display='none';
     document.getElementById("home").style.display='block';
+    
 },2000);
 
 document.getElementById("essenza").onclick = function () {
-    location.href = "./studio2/index.html";
+
 };
 
 document.getElementById("sto").onclick = function () {
     location.href = "./studio2/sto.html";
+
 };
+
 
 })();
---
+++ des157a/final/usertest/script.js
@@ -0,0 +1,20 @@
+(function () {
+
+setTimeout(function(){
+    document.getElementById("start").style.display='none';
+    document.getElementById("home").style.display='block';
+    alert("Click the car you like");
+},2000);
+
+document.getElementById("essenza").onclick = function () {
+    location.href = "./studio2/index.html";
+    alert("Slide or Click the dots for different views of the car. The hard slide will slide to next view while the soft slide will move the view.");
+};
+
+document.getElementById("sto").onclick = function () {
+    location.href = "./studio2/sto.html";
+    alert("Slide or Click the dots for different views of the car. The hard slide will slide to next view while the soft slide will move the view.");
+};
+
+
+})();
